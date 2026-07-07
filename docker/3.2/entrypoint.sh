7bd33790e55d4abc2ebd337dc2e2037d3700c0b5
purple grape <purplegrape4@gmail.com>
2026-07-07 17:37:19 +0800

    Update entrypoint.sh

--- docker/3.2/entrypoint.sh
+++ docker/3.2/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ ! -d /data/itop ];then
+if [ ! -d /data/itop/setup ];then
   rsync -aqu /usr/src/itop/ /data/itop/
   mkdir -p /data/itop/{conf,data,log,env-production,env-production-build}
   chown -R www-data:www-data /data/itop/{conf,data,log,env-production,env-production-build}
